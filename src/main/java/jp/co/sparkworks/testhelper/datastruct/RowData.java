b810a586683dbda18650efce7c523a8536b85991
sohrichoh <30592379+sohrichoh@users.noreply.github.com>
2017-11-02 18:07:14 +0900

    Create RowData.java

---
+++ src/main/java/jp/co/sparkworks/testhelper/datastruct/RowData.java
@@ -0,0 +1,24 @@
+package jp.co.sparkworks.testhelper.datastruct;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RowData {
+
+    private List<ColumnData> columnData = new ArrayList<ColumnData>();
+
+
+    public List<ColumnData> getColumnData() {
+        return columnData;
+    }
+
+
+    public void setColumnData(List<ColumnData> columnData) {
+        this.columnData = columnData;
+    }
+
+
+    public String toString() {
+        return columnData.toString();
+    }
+
+}
