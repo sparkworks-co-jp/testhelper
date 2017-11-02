70c3b4829576b8e07de5776927b2cb57ff188306
sohrichoh <30592379+sohrichoh@users.noreply.github.com>
2017-11-02 18:06:14 +0900

    Create DBHelper.java

---
+++ src/main/java/jp/co/sparkworks/testhelper/DBHelper.java
@@ -0,0 +1,86 @@
+package jp.co.sparkworks.testhelper;
+
+import java.util.ArrayList;
+import java.util.List;
+import jp.co.sparkworks.testhelper.datastruct.TableData;
+import jp.co.sparkworks.testhelper.dbaccess.DBExecutor;
+import jp.co.sparkworks.testhelper.generater.CSVGenerater;
+import jp.co.sparkworks.testhelper.generater.SQLGenerater;
+import jp.co.sparkworks.testhelper.input.Reader;
+import jp.co.sparkworks.testhelper.output.Writer;
+
+public class DBHelper {
+
+    public static void generatarSQL(String... tableNames) throws Throwable {
+
+        for (String tableName : tableNames) {
+            System.out.println("[" + tableName + "] InsertSQL ");
+
+            // 全データ取得
+            TableData table = DBExecutor.executeQueryByTableName(tableName);
+
+            if (table == null || table.getTableData().size() == 0) {
+                System.out.println("NO DATA!");
+            } else {
+                // SQL作成
+                String insertSQL = SQLGenerater.generater(table);
+
+                // ファイル出力
+                System.out.println(insertSQL);
+                Writer.writerSQL(tableName, insertSQL);
+            }
+
+        }
+    }
+
+
+    public static void generatarCSV(String... tableNames) throws Throwable {
+
+        for (String tableName : tableNames) {
+            System.out.println("[" + tableName + "] CSV ");
+
+            // 全データ取得
+            TableData table = DBExecutor.executeQueryByTableName(tableName);
+
+            if (table == null || table.getTableData().size() == 0) {
+                System.out.println("NO DATA!");
+            } else {
+                // CSV作成
+                String csvString = CSVGenerater.generater(table);
+                System.out.println(csvString);
+
+                // ファイル出力
+                Writer.writerCSV(tableName, csvString);
+            }
+
+        }
+    }
+
+
+
+    public static void executeSQL(String foldername) throws Throwable {
+        // 全SQL取得
+        List<String> list = Reader.readSQL(foldername);
+
+        // SQL実施
+        List<String> sqls = new ArrayList<String>();
+        for (String sql : list) {
+            String exesql = sql.trim();
+            if (exesql.endsWith(";")) {
+                exesql.replace(";", "");
+            }
+            sqls.add(exesql);
+        }
+
+
+        DBExecutor.executeUpdate(sqls);
+
+    }
+
+    public static void clearTables(String... tables) throws Throwable {
+        for (String tablename : tables) {
+            String sql = "delete from " + tablename;
+            DBExecutor.executeUpdate(sql);
+        }
+    }
+}
