ca019fdbeb6c896d10ecc142f229dd7e6a9d6648
sohrichoh <30592379+sohrichoh@users.noreply.github.com>
2017-11-02 18:06:35 +0900

    Create ColumnData.java

---
+++ src/main/java/jp/co/sparkworks/testhelper/datastruct/ColumnData.java
@@ -0,0 +1,48 @@
+package jp.co.sparkworks.testhelper.datastruct;
+
+public class ColumnData {
+
+    private String name;
+    private String value;
+    private ColumnType columnType;
+    private boolean isAutoIncrement;
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public ColumnType getColumnType() {
+        return columnType;
+    }
+
+    public void setColumnType(ColumnType columnType) {
+        this.columnType = columnType;
+    }
+
+    public boolean isAutoIncrement() {
+        return isAutoIncrement;
+    }
+
+    public void setAutoIncrement(boolean isAutoIncrement) {
+        this.isAutoIncrement = isAutoIncrement;
+    }
+
+    public String toString() {
+        return name + "-" + value + "-" + columnType + "-" + isAutoIncrement;
+    }
+
+}
