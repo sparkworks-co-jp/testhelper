d89722dcaf404c72547ebf7605e04c72fd21fc1a
sohrichoh <30592379+sohrichoh@users.noreply.github.com>
2017-11-02 18:07:30 +0900

    Create TableData.java

---
+++ src/main/java/jp/co/sparkworks/testhelper/datastruct/TableData.java
@@ -0,0 +1,35 @@
+package jp.co.sparkworks.testhelper.datastruct;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TableData {
+
+    private String tableName;
+    private List<RowData> tableData = new ArrayList<RowData>();
+
+
+    public String getTableName() {
+        return tableName;
+    }
+
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+
+
+    public List<RowData> getTableData() {
+        return tableData;
+    }
+
+
+    public void setTableData(List<RowData> tableData) {
+        this.tableData = tableData;
+    }
+
+
+    public String toString() {
+        return tableName + "\n" + tableData.toString();
+    }
+
+}
