5affa528fc76bf95ad1466bb8d53cb742ba16250
sohrichoh <30592379+sohrichoh@users.noreply.github.com>
2017-11-02 18:06:50 +0900

    Create ColumnType.java

---
+++ src/main/java/jp/co/sparkworks/testhelper/datastruct/ColumnType.java
@@ -0,0 +1,24 @@
+package jp.co.sparkworks.testhelper.datastruct;
+public enum ColumnType {
+    DIGIT(1), STRING(2), DATETIME(3);
+
+    private final int id;
+
+    private ColumnType(final int id) {
+        this.id = id;
+    }
+
+    public static ColumnType valueOf(int id) {
+
+        for (ColumnType num : values()) {
+            if (num.getId() == id) {
+                return num;
+            }
+        }
+        throw new IllegalArgumentException("no such enum object for the id: " + id);
+    }
+
+    public int getId() {
+        return id;
+    }
+}
