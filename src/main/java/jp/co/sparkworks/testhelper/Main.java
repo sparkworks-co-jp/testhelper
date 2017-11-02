897f6e835d3bf031e039d486d7fa3665c66f29fe
sohrichoh <30592379+sohrichoh@users.noreply.github.com>
2017-11-02 18:04:05 +0900

    Create Main.java

---
+++ src/main/java/jp/co/sparkworks/testhelper/Main.java
@@ -0,0 +1,26 @@
+package jp.co.sparkworks.testhelper;
+
+public class Main {
+
+    public static void main(String[] args) throws Throwable {
+
+        String[] targetTables = { //
+                "aaa", //
+                "bbb"//
+        };
+
+        //   DBHelper.clearTables(targetTables);
+
+       dothat(targetTables);
+
+       //   DBHelper.executeSQL("times-20171102175245");
+    }
+
+    // 両方出力
+    static void dothat(String... tableNames) throws Throwable {
+
+        DBHelper.generatarSQL(tableNames);
+
+        DBHelper.generatarCSV(tableNames);
+    }
+}
