4b476774cd72886f80174fe2a7ee5bf4eeed8185
sohrichoh <30592379+sohrichoh@users.noreply.github.com>
2017-11-02 18:09:03 +0900

    Create Reader.java

---
+++ src/main/java/jp/co/sparkworks/testhelper/input/Reader.java
@@ -0,0 +1,59 @@
+package jp.co.sparkworks.testhelper.input;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Reader {
+
+
+    private static String FOLDER = "./output";
+
+    public static List<String> readSQL(String foldername) throws Throwable {
+
+        List<String> list = new ArrayList<String>();
+
+        File dir = new File(FOLDER, foldername);
+
+        if (!dir.exists() || !dir.isDirectory()) {
+            System.out.println("フォルダ[" + dir.getAbsolutePath() + "]存在していません！");
+        }
+
+        FilenameFilter filter = new FilenameFilter() {
+            public boolean accept(File file, String str) {
+
+                // 拡張子を指定する
+                if (str.toLowerCase().endsWith("sql")) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        };
+
+
+        File[] files = dir.listFiles(filter);
+
+        for (File file : files) {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+
+            String sql = br.readLine();
+            while (sql != null) {
+                System.out.println(sql);
+                list.add(sql);
+                sql = br.readLine();
+            }
+
+            br.close();
+
+        }
+
+        return list;
+
+    }
+
+
+}
