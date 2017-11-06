1fb01e053efac64ca4060994b9c3135088e6e77d
sohrichoh <30592379+sohrichoh@users.noreply.github.com>
2017-11-06 12:26:23 +0900

    Update Writer.java

--- src/main/java/jp/co/sparkworks/testhelper/output/Writer.java
+++ src/main/java/jp/co/sparkworks/testhelper/output/Writer.java
@@ -1,4 +1,5 @@
 package jp.co.sparkworks.testhelper.output;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.time.LocalDateTime;
@@ -9,37 +10,46 @@ public class Writer {
 
     private static String FOLDER = "./output";
 
+    // 一回の実施でフォルダ名を固定するため、staticに定義している
+    private static String timestamp = null;
+
     public static void writerSQL(String tableName, String contents) throws Throwable {
+        String foldername = getFolderName("SQL");
         String filename = tableName + ".sql";
-        writerToFile(filename, contents);
+        File file = new File(foldername, filename);
+        writerToFile(file, contents);
     }
 
     public static void writerCSV(String tableName, String contents) throws Throwable {
+        String foldername = getFolderName("CSV");
         String filename = tableName + ".csv";
-        writerToFile(filename, contents);
+        File file = new File(foldername, filename);
+        writerToFile(file, contents);
     }
 
-    private static void writerToFile(String filename, String contents) throws Throwable {
-
-        String foldername = getFolderName();
-        File folder = new File(foldername);
-        if (!folder.exists()) {
-            folder.mkdirs();
-        }
-
-        File file = new File(getFolderName(), filename);
-
+    private static void writerToFile(File file, String contents) throws Throwable {
         FileWriter filewriter = new FileWriter(file);
-
         filewriter.write(contents);
         filewriter.flush();
         filewriter.close();
     }
 
-    private static String getFolderName() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
-        String timestamp = LocalDateTime.now().format(formatter).toString();
-        String foldername = FOLDER + "/times-" + timestamp;
-        return foldername;
+    private static String getFolderName(String prefix) {
+        String folderName = FOLDER + "/" + prefix + "-" + getTimeStamp();
+        File folder = new File(folderName);
+        if (!folder.exists()) {
+            folder.mkdirs();
+        }
+
+        return folderName;
     }
+
+    private static String getTimeStamp() {
+        if (timestamp == null) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+            timestamp = LocalDateTime.now().format(formatter).toString();
+        }
+        return timestamp;
+    }
+
 }
